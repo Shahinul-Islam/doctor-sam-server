6528088e119ae718be03725ba0acc4d39a3fe177
shahinul islam <shahinul37@outlook.com>
2022-11-08 07:12:40 +0600

    data base user and password setup done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,8 +9,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_PASSWORD);
+
 app.get("/", (req, res) => {
   res.send("doctor sam server is running");
+});
+
+//database connection
+
+// user_name:doctorSamDb
+//password:NC5CWFIoL61jBw5g
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kdwgcoc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const serviceCollection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
 app.listen(port, () => {
